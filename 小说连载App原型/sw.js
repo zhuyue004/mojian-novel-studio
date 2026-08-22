d5a11bbca5a38c706d84a7e0132448c043c3127e
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-22 23:50:44 +0800

    Simplify bottom navigation

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v2';
+const CACHE_NAME = 'mojian-v3';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
