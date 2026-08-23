1705974cdd3648bf8858768abdd6c4e6b17a3df5
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 12:33:20 +0800

    Add system dark mode

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v32';
+const CACHE_NAME = 'mojian-v33';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
