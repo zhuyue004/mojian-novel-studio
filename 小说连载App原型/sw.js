e689695f723e65a86addc3398cc6ed432cac5368
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 14:35:38 +0800

    Pin mobile navigation to viewport

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v36';
+const CACHE_NAME = 'mojian-v37';
 const APP_SHELL = ['./', './index.html', './styles.css', './dark.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
