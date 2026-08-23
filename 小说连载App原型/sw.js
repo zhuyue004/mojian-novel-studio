37e691bd5743c3e7018cdcc5f4fac03dca8cdf42
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 16:22:51 +0800

    Tighten mobile safe area spacing

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v40';
+const CACHE_NAME = 'mojian-v41';
 const APP_SHELL = ['./', './index.html', './styles.css', './dark.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
