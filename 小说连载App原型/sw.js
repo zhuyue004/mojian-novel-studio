0eccd658bb221f1e5ca7611697e94169eb03c549
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 18:43:17 +0800

    Show outline form only on demand

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v57';
+const CACHE_NAME = 'mojian-v58';
 const APP_SHELL = ['./', './index.html', './styles.css', './dark.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
