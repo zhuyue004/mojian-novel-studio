6b6945b4c14f268fc1db93595aff5470974c14fa
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 17:48:58 +0800

    Make tool guidance toast clearly visible

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v53';
+const CACHE_NAME = 'mojian-v54';
 const APP_SHELL = ['./', './index.html', './styles.css', './dark.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
