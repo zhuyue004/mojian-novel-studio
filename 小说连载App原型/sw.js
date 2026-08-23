bffd09d8712bfe9ea119962b19e24c85b76cbf90
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 16:45:49 +0800

    Move mobile content closer to status bar

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v43';
+const CACHE_NAME = 'mojian-v44';
 const APP_SHELL = ['./', './index.html', './styles.css', './dark.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
