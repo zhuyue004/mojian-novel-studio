fe5633b641968edf947f5f8c6a5cf58a9c565e03
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 09:30:43 +0800

    Simplify chapter switch control

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v12';
+const CACHE_NAME = 'mojian-v13';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
