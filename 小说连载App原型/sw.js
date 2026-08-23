2b334a608c46ba4ce54a2dd4baa8cdaa93c87a80
ZhuYue <zhuyue004@users.noreply.github.com>
2026-08-23 16:56:11 +0800

    Unify standalone status bar color

--- 小说连载App原型/sw.js
+++ 小说连载App原型/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mojian-v45';
+const CACHE_NAME = 'mojian-v46';
 const APP_SHELL = ['./', './index.html', './styles.css', './dark.css', './app.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
